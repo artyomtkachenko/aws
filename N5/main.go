b96b2deb841a1881d2646f385bfceb5c6f5f8788
Tim <timtkachenko@nbnco.com.au>
2015-08-14 20:52:37 +1000

    A better version, but still hass bugs

--- N5/main.go
+++ N5/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sort"
 	"time"
 )
 
@@ -28,38 +27,65 @@ func (p byPoints) Less(i, j int) bool {
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
+func genRand() points {
+	n := 100
+	p := new(points)
+	p.x = rand.Intn(n)
+	p.y = rand.Intn(n)
+	p.d = p.x*p.x + p.y*p.y
+	if p.x == 0 || p.y == 0 {
+		*p = genRand()
+	}
+	return *p
+}
 
 func generate(queue chan points) {
-	for i := 0; i < 10; i++ {
-		n := 100
-		p := new(points)
-		p.x = rand.Intn(n)
-		p.y = rand.Intn(n)
-		p.d = p.x*p.x + p.y*p.y
-		queue <- *p
+	quit := make(chan bool)
+	timer := time.NewTimer(time.Millisecond * 20)
+	go func() {
+		<-timer.C
+		fmt.Println("Timer expired")
+		quit <- true
+	}()
+	for {
+		select {
+		case <-quit:
+			// time.Sleep(9000 * time.Millisecond) // Gi
+			fmt.Println("Got QUIT")
+			close(queue)
+			return
+		default:
+			queue <- genRand()
+		}
 	}
+
 }
 
 func reduce(queue chan points, k int) {
 	var arr byPoints
-	for i := 0; i < 10; i++ {
-		p := <-queue
-		fmt.Println("Got: ", p)
+
+	for p := range queue {
+
+		// fmt.Printf("Got: %+v\n", p)
 		if len(arr) < k {
 			arr = append(arr, p)
 		} else {
-			sort.Sort(arr)
+			// fmt.Printf("Before sort Got: %+v\n", arr)
+			// sort.Sort(arr)
+			// fmt.Printf("After sirt Got: %+v\n", arr)
 			last := arr[k-1]
 			if p.d <= last.d {
 				arr = append(arr[:k-1], p) // Replacing the max one
 			}
 		}
 	}
-	fmt.Println(arr)
+	fmt.Printf("Got: %+v\n", arr)
+	// fmt.Println(arr)
 }
 
 func main() {
 	queue := make(chan points) // Buffered channel for a stream
+
 	k := 2
 	go generate(queue)
 	reduce(queue, k)
