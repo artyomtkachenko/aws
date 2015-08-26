295c38e0bc695671ded0fa3f89a9ee117ab40f8f
Tim <timtkachenko@nbnco.com.au>
2015-08-26 17:06:29 +1000

    Playing with dynamic prog

--- N6/main.go
+++ N6/main.go
@@ -1,12 +1,47 @@
 package main
 
-import "github.com/artyomtkachenko/aws/subsum"
+import "fmt"
+
+func debug(arr [][]bool) {
+	for i, el := range arr {
+		fmt.Println(i, el)
+	}
+}
+
+func dynamic(set []int, sum int) {
+	size := len(set)
+
+	subset := make([][]bool, size)
+
+	for i := 0; i <= size; i++ {
+		subset[i] = make([]bool, sum+1) // we need it to start from 0
+		subset[i][0] = true
+		for j := 1; j <=sum; j++ {
+			
+		}
+	}
+	for i := 1; i <= sum; i++ {
+		// subset[i] = make([]bool, size+1)
+		// subset[i][0] = false
+	}
+
+	for i := 1; i <= sum; i++ {
+		for j := 1; j <= size; j++ {
+			subset[i][j] = subset[i][j-1]
+			if i >= set[j-1] {
+				subset[i][j] = subset[i][j] || subset[i-set[j-1]][j-1]
+			}
+		}
+	}
+	debug(subset)
+}
 
 func main() {
-	data := []int{5, 5, 15, 2, 3, 10}
+	data := []int{5, 5, 15, 10}
 	sum := 15
-	res := subsum.GetResult(data, sum)
-	for _, el := range res {
-		subsum.PrintResult(el, sum)
-	}
+	// _ = subsum.GetResult(data, sum)
+	// for _, el := range res {
+	// 	subsum.PrintResult(el, sum)
+	// }
+	dynamic(data, sum)
 }
